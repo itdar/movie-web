aaad15a1ac852cb7091d74c78e9c46ff84381e99
itdar <thesockerrr@gmail.com>
2021-01-03 13:33:59 +0900

    App.js 파일 수정, component 연결 및 render 기능 확인 함.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+    
+  };
+  constructor(props) {
+    super(props);
+    console.log("App component constructor");
+  }
+  add = () => {
+    this.setState(current => ({count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("component is updated");
+  }
+  componentWillUnmount() {
+    console.log("component will be unmounted")
+  }
+  render() {
+    console.log("component rendering in render()");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
